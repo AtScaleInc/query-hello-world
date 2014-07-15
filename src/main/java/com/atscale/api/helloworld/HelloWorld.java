839840e03b9a6f98231ae94dc59f9c7abc8113d8
David Ross <dyross@gmail.com>
2014-07-14 18:19:45 -0700

    Initial Commit

---
+++ src/main/java/com/atscale/api/helloworld/HelloWorld.java
@@ -0,0 +1,48 @@
+package com.atscale.api.helloworld;
+
+import java.sql.*;
+
+class HelloWorld {
+
+  private static String SQL_QUERY =
+    "SELECT `Sales`.`Country` AS `country`,\n" +
+    "SUM(`Sales`.`Sales Amount`) AS `sales_amount`\n" +
+    "FROM `Sample`.`Sales` `Sales`\n" +
+    "GROUP BY `Sales`.`Country`";
+
+  private static String SQL_EXPORT =
+    "CREATE TABLE %s AS (\n" +
+    "SELECT `Sales`.`Country` AS `country`,\n" +
+    "SUM(`Sales`.`Sales Amount`) AS `sales_amount`\n" +
+    "FROM `Sample`.`Sales` `Sales`\n" +
+    "GROUP BY `Sales`.`Country`\n" +
+    ") WITH DATA";
+
+  // $ mvn compile exec:java -Dexec.mainClass="com.atscale.api.helloworld.HelloWorld" -Dexec.args="192.168.99.9 test_export"
+  // Reference: https://cwiki.apache.org/confluence/display/Hive/HiveServer2+Clients
+  public static void main(String args[]) throws SQLException, ClassNotFoundException, InterruptedException {
+    Class.forName("org.apache.hive.jdbc.HiveDriver");
+
+    String host = args[0];
+    String connectionUrl = String.format("jdbc:hive2://%s:11111", host);
+    System.out.println("About to connect to: " + connectionUrl);
+    Connection connection = DriverManager.getConnection(connectionUrl, "admin", "admin");
+    Statement statement = connection.createStatement();
+    ResultSet resultSet = null;
+
+    System.out.println("\n\nAbout to execute query:\n\n" + SQL_QUERY);
+    resultSet = statement.executeQuery(SQL_QUERY);
+    System.out.println("\n\nQuery results:\n");
+    while (resultSet.next()) {
+      System.out.println(resultSet.getString(1) + "\t" + resultSet.getDouble(2));
+    }
+
+    String createTableName = args[1] + "_" + new java.util.Date().getTime();
+    String sqlExport = String.format(SQL_EXPORT, createTableName);
+    System.out.println("\n\nAbout to execute export:\n\n" + sqlExport);
+
+    resultSet = statement.executeQuery(sqlExport);
+    resultSet.next();
+    System.out.println("\n\nTable created!");
+  }
+}
